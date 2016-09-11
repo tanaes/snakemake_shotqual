99701f7f0f66589ad751275d0439fb4b8ba93bb6
Jon Sanders <jsanders@oeb.harvard.edu>
2016-09-11 16:44:37 -0400

    Added cluster launch files

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "script.sh <out_dir (with config.yaml)> [additional snakemake arguments]*"
+    exit
+fi
+
+snakemake -j 4 --local-cores 4 --cluster-config cluster.json --cluster "qsub -k eo -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@"
+#snakemake --directory ~/pipeline_test/exp1/ --dag | dot -Tsvg > dag.svg 
+#-o /home/snurk/logs/ -e /home/snurk/logs/ 
